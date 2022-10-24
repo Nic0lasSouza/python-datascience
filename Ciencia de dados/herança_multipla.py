377070f015ad7c95c925c0e9f799ba051751233b
Nicolas Souza <nicolasdsmdsouza2004@gmail.com>
2022-10-24 13:53:02 -0300

    alterado

--- Ciencia de dados/herança_multipla.py
+++ Ciencia de dados/herança_multipla.py
@@ -6,14 +6,18 @@ class Animal:
         return f"{self.__class__.__name__}:{', '.join([f'{chave} = {valor}' for chave, valor in self.__dict__.items()])}"
 
 class Mamifero(Animal):
-    def __init__(self, numero_patas) :
-        self.numero_patas = numero_patas
-        super().__init__(numero_patas)
+    def __init__(self, cor_pelo, **kw) :
+        self.cor_pelo = cor_pelo
+        super().__init__(**kw)
+
 
 class Ave(Animal):
-    def __init__(self, numero_patas) :
-        self.numero_patas = numero_patas
-        super().__init__(numero_patas)
+    def __init__(self, cor_bico, **kw):
+        self.cor_bico = cor_bico
+        super().__init__(**kw)
+
+    def __str__(self):
+        return "Ave 42"
 
 class Cachorro(Mamifero):
     pass
@@ -21,12 +25,22 @@ class Cachorro(Mamifero):
 class Gato(Mamifero):
     pass
 
-class Leao(Mamifero):
-    pass
+# class Leao(Mamifero):
+#     pass
 
 
 class Ornintorrinco(Mamifero, Ave):
-    pass
+    def __init__(self, cor_bico, cor_pelo, numero_patas):
+        print(Ornintorrinco.__mro__)
+        super().__init__(cor_pelo=cor_pelo, cor_bico=cor_bico, numero_patas=numero_patas)
 
-gato = Gato(4)
+gato = Gato(numero_patas = 4, cor_pelo = "preto")
 print(gato)
+
+
+Ornintorrinco = Ornintorrinco(numero_patas = 2, cor_pelo = "vermelho", cor_bico = "laranja")
+print(Ornintorrinco)
+
+
+
+#olhar depois ás 18:30
