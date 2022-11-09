785f6abad2b65aa74babb76e02380dca3505b416
Nicolas Souza <NicolasdsmdSouza2004@gmail.com>
2022-11-09 13:39:39 -0300

    alterado

--- u.py
+++ u.py
@@ -1,7 +1,17 @@
-soma = 0 
-while True:
-    x = int(input("Digite op numero zero :"))
-    if x == 0:
-        break
-    soma = soma+x
-print("Soma: %d" %soma)
+from tkinter import Tk, Frame, Button
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
+ventana = Tk()
+ventana.geometry('655x505')
+ventana.wm_title('Gráfico Circular')
+ventana.minsize(width=655, height=505)
+frame=Frame(ventana, bg='gray')
+frame.grid(column=0, row=0, sticky='nsew')
+
+idades = ['25 a 34','35 a 44','45 a 54', '18 a 24']
+ccolor = ['green', 'yellow', 'red', 'blue']
+tamanho = [5.6, 11.1, 16.7, 66.7]
+explotar = [0.5, 0.5, 0.5, 0.5]
+
+fig, ax1 = plt.subplot()
